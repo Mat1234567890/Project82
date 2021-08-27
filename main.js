eb4854518d248c4ce5b6bd054f163203be702c74
Mat1234567890 <78577538+Mat1234567890@users.noreply.github.com>
2021-08-26 21:53:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 var Canvas= document.getElementById("myCanvas");
 ctx= Canvas.getContext("2d");
 
-var Enabled=true;
-function Radius_windowalert(){
-if(Enabled == true){window.alert("Enter the radius you want for your circle in the radius input box please");}
-Enabled=false;
-}
-
 var mouse_event="empty";
 var last_pos_x,last_pos_y;
 var color="black";
-var radius=35;
 var width_of_line="5";
 
 Canvas.addEventListener("mousedown",my_mousedown);
